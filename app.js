66ce671427ea5ded70adaaaee2ff71bbea2033bc
Kevin Oviedo <kevinoviedo2018@gmail.com>
2021-11-15 23:14:13 -0300

    ultimos arreglos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ addEventListener('DOMContentLoaded', () =>{
     if (btn_menu){
         btn_menu.addEventListener ('click', () =>{
             const menu_items = document.querySelector('.menu_items')
-            menu_items.classList.toggle('Show')
+            menu_items.classList.toggle('show')
         })
     }
 })
